0b5036666a923911c085b2192ed0269b63c0b1c0
alvikag <alvikag@iiitd.ac.in>
2022-10-18 13:09:45 -0500

    fixes for out of range, 3 anchors

--- src/decawave_ros/__init__.py
+++ src/decawave_ros/__init__.py
@@ -6,24 +6,27 @@ DecaMsg = namedtuple('DecaMsg', 'typ anc tag r0 r1 r2 r3')
 
 
 class DecaParser:
-    def __init__(self, stream):
+    def __init__(self,stream):
         self.stream = stream
 
 
     def receive(self):
 
         data = self.stream.readline().decode('utf-8').split()
+        #print(data)
+        try:
+            if (data[0] == 'mc'):
+                if(data[9][0] == "a"):
+                    typ = 'Anchor'
+                else:
+                    typ = 'Tag'
+                anc = int(data[9][3])
+                tag = int(data[9][1])
+                r0  = int(data[2],16)/1000.0
+                r1  = int(data[3],16)/1000.0
+                r2  = int(data[4],16)/1000.0
+                r3  = int(data[5],16)/1000.0
 
-        if (data[0] == 'mc' and not int(data[1],16) == 7):
-            if(data[9][0] == "a"):
-                typ = 'Anchor'
-            else:
-                typ = 'Tag'
-            anc = int(data[9][3])
-            tag = int(data[9][1])
-            r0  = int(data[2],16)/1000.0
-            r1  = int(data[3],16)/1000.0
-            r2  = int(data[4],16)/1000.0
-            r3  = int(data[5],16)/1000.0
-
-            return DecaMsg(typ, anc, tag, r0, r1, r2, r3)
+                return DecaMsg(typ, anc, tag, r0, r1, r2, r3)
+        except:
+            return
