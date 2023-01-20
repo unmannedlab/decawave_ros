5601889785f5d7a4e5506f8ff79c2cc5a97350f2
alvikag <alvikag@iiitd.ac.in>
2023-01-20 16:01:04 -0600

    minor modify for 4 anchors

--- src/decawave_ros/__init__.py
+++ src/decawave_ros/__init__.py
@@ -2,7 +2,7 @@
 
 from collections import namedtuple
 
-DecaMsg = namedtuple('DecaMsg', 'typ anc tag r0 r1 r2 r3')
+DecaMsg = namedtuple('DecaMsg', 'tag r0 r1 r2 r3')
 
 
 class DecaParser:
@@ -13,20 +13,23 @@ class DecaParser:
     def receive(self):
 
         data = self.stream.readline().decode('utf-8').split()
-        #print(data)
+        #data=[]
+        print(data)
         try:
             if (data[0] == 'mc'):
-                if(data[9][0] == "a"):
+                '''
+                if(data[9][0] == "a"):    # actually always "a", no other letter
                     typ = 'Anchor'
                 else:
                     typ = 'Tag'
-                anc = int(data[9][3])
-                tag = int(data[9][1])
+                anc = int(data[9][3])   # the number is always 0, it is not anchor id 
+                '''
+                tag = int(data[9][1])   # if there is one tag, the number is 0 for t0, if there are two tags, the number for t1 is 1, likewise, 2 is for t2, 3 is for t3.
                 r0  = int(data[2],16)/1000.0
                 r1  = int(data[3],16)/1000.0
                 r2  = int(data[4],16)/1000.0
                 r3  = int(data[5],16)/1000.0
 
-                return DecaMsg(typ, anc, tag, r0, r1, r2, r3)
+                return DecaMsg(tag, r0, r1, r2, r3)
         except:
             return
